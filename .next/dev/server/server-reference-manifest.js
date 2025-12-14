2eb009161e8ceb3d313d64daa27c1e2465a1d5bc
vito-ysl <vitorevangelista0557@gmail.com>
2025-12-13 23:03:04 -0300

    Home: Construindo portifolio.

---
+++ .next/dev/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"7S8BXGOI+YRBhvZ8fGYXPJAudK0T8F3qDOtbKDM6Dhw=\"\n}"
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
 export default function Page() {
-  return <h1>Vitor Evangelista</h1>;
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <h1 className="text-center">Vitor Evangelista Desenvolvedor</h1>
+    </div>
+  );
 }
